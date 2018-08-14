0ebf0c734842a324e77be3321dae4ea1ea91a100
Magnus Persson <magnus.persson@my.jcu.edu.au>
2018-08-14 12:10:32 +1000

    New functionality to password_entry checking for min length

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,17 @@
 """
 Magnus Persson
 """
+def main():
+
+    valid = False
+    MIN_LENGTH = 7
+    while not valid:
+        user_password = input('Password')
+
+        if len(user_password) >= MIN_LENGTH:
+            valid = True
+            print("ALL GOOD")
+        else:
+            print("NOT ENOUGH LENGTH")
+
+main()
